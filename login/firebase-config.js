4a510a383c71bfca8ebca72ef6d121ec5ce18fef
musichub <114087590+picshub@users.noreply.github.com>
2024-03-29 00:45:35 +0100

    Delete login directory

--- login/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Your Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAM6fIwGYbBDVw80RgnRjZLb1MxWconWNQ",
-    authDomain: "login-2453c.firebaseapp.com",
-    projectId: "login-2453c",
-    storageBucket: "login-2453c.appspot.com",
-    messagingSenderId: "322381381766",
-    appId: "1:322381381766:web:bccc7675ff8b8a662f5107"
-};
